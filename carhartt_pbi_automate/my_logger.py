72ffc07c97b87479b249af1d920c64d81f0353fe
Jorge Ricardo Escobar Carrasco <rescobar@carhartt.com>
2024-01-15 16:59:02 -0500

    Refactor MyLogger class to add more logging options

--- carhartt_pbi_automate/my_logger.py
+++ carhartt_pbi_automate/my_logger.py
@@ -1,32 +1,81 @@
 """This module creates a logger for the application."""
 import logging
 from pathlib import Path
+from typing import Union
+
+from carhartt_pbi_automate.database import Database
+from carhartt_pbi_automate.sqlite_handler import SqliteHandler
 
 
 class MyLogger(logging.Logger):
     """This class represents the logger for the application."""
 
-    def __init__(self, name: str, log_file: Path, level: int = logging.INFO):
+    def __init__(
+        self,
+        name: str,
+        log_file: Path,
+        level: int = logging.INFO,
+        log_to_console: bool = True,
+        log_to_file: bool = True,
+        log_to_database: bool = True,
+        database: Union[Database, str] = None,
+    ):
         """Initialize the logger."""
         super().__init__(name, level)
         self.log_file = log_file
         self.log_file.parent.mkdir(parents=True, exist_ok=True)
 
         # Create a file handler to write to the log file
-        self.file_handler = logging.FileHandler(self.log_file)
-        self.file_handler.setLevel(level)
-        self.file_formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
-        self.file_handler.setFormatter(self.file_formatter)
+        if log_to_file:
+            self.file_handler = logging.FileHandler(self.log_file)
+            self.file_handler.setLevel(level)
+            self.file_formatter = logging.Formatter(
+                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            )
+            self.file_handler.setFormatter(self.file_formatter)
+            self.addHandler(self.file_handler)
+            self.info("File logging enabled")
 
         # Create a stream handler to print to stdout
-        self.stream_handler = logging.StreamHandler()
-        self.stream_handler.setLevel(level)
-        self.stream_formatter = logging.Formatter("%(message)s")
-        self.stream_handler.setFormatter(self.stream_formatter)
-        self.addHandler(self.file_handler)
-        self.addHandler(self.stream_handler)
+        if log_to_console:
+            self.stream_handler = logging.StreamHandler()
+            self.stream_handler.setLevel(level)
+            self.stream_formatter = logging.Formatter("%(message)s")
+            self.stream_handler.setFormatter(self.stream_formatter)
+            self.addHandler(self.stream_handler)
+            self.info("Console logging enabled")
+
+        # Create a database handler to write to a database
+        if log_to_database:
+            if database:
+                if isinstance(database, Database):
+                    self.database = database
+                elif isinstance(database, str):
+                    database_path = Path(database)
+                    self.database = Database(database_path)
+                else:
+                    raise TypeError("database must be a Database or str")
+            else:
+                DEFAULT_DATABASE = "database/logging.db"
+                database_path = Path(DEFAULT_DATABASE)
+                database_path.parent.mkdir(parents=True, exist_ok=True)
+                try:
+                    self.database = Database(database_path)
+                except Exception as exption:
+                    self.warning(
+                        "Unable to open database at %s: %s",
+                        database_path,
+                        exption
+                    )
+                    # Create an empty database object
+                    self.database = Database(":memory:")
+                    self.database = None
+
+            self.database = database
+            self.sqlite_handler = SqliteHandler(self.database)
+            self.sqlite_handler.setLevel(level)
+            self.addHandler(self.sqlite_handler)
+            self.info("Database logging enabled")
 
         self.info("Logger initialized")
 
@@ -37,9 +86,3 @@ class MyLogger(logging.Logger):
         self.file_handler.close()
         self.removeHandler(self.stream_handler)
         self.stream_handler.close()
-
-if __name__ == "__main__":
-    # This code is executed when the file is run from the command line
-    logger = MyLogger("test", Path("test.log"))
-    logger.info("This is a test")
-    logger.close()
