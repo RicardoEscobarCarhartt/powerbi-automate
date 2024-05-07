2e45c43818acb6354d806348ecd3b85d314f6d50
Jorge Ricardo Escobar Carrasco <rescobar@carhartt.com>
2024-05-07 10:52:39 -0400

    Update data extraction time logging in app.py

--- carhartt_pbi_automate/app.py
+++ carhartt_pbi_automate/app.py
@@ -112,7 +112,9 @@ time_start = datetime.now()
 print("Extracting data from EDW...")
 df_edw = pd.read_sql(query_edw, conn_EDW)
 time_end = datetime.now()
-print(f"Time to extract data from EDW: {time_end - time_start}")
+time_diff = time_end - time_start
+# print(f"Time to extract data from EDW: {time_diff}")
+print(f"Time to extract data from EDW: {time_diff.seconds // 3600} hours {(time_diff.seconds // 60) % 60} minutes {time_diff.seconds % 60} seconds")
 print("Data from EDW has been extracted!")
 
 #################### (2) DATASET BI DASHBOARD ####################
@@ -221,7 +223,8 @@ data_table = list(results_table)
 df_bi = pd.DataFrame(data_table, columns=column_names)
 cursor_data_BI.close()
 time_end = datetime.now()
-print(f"Time to extract data from Power BI: {time_end - time_start}")
+time_diff = time_end - time_start
+print(f"Time to extract data from Power BI: {time_diff.seconds // 3600} hours {(time_diff.seconds // 60) % 60} minutes {time_diff.seconds % 60} seconds")
 print("Data from Power BI has been extracted!")
 
 # Modify the column name to match the EDW dataset
