5c4ddb67d3c5bf7025ad797fcc71308edc635a28
Jorge Ricardo Escobar Carrasco <rescobar@carhartt.com>
2024-05-20 12:10:27 -0400

    Update connector.py to include type hints and docstrings for get_edw_connection() and get_bi_connection() functions

--- carhartt_pbi_automate/connector.py
+++ carhartt_pbi_automate/connector.py
@@ -6,22 +6,32 @@ from sqlalchemy import create_engine
 
 
 def get_edw_connection(
-    server="DBNSQLQNET",
-    database="CarharttDw",
-    driver="ODBC Driver 17 for SQL Server",
-):
-    """Returns a connection to the CarharttDw database."""
+    server: str = "DBNSQLQNET",
+    database: str = "CarharttDw",
+    driver: str = "ODBC Driver 17 for SQL Server",
+) -> adodbapi.Connection:
+    """Returns a connection to the CarharttDw database.
+    Args:
+        server (str): The server name.
+        database (str): The database name.
+        driver (str): The driver name.
+    Returns:
+        adodbapi.Connection: The connection to the CarharttDw database."""
     connection_string = f"mssql+pyodbc://{server}/{database}?driver={driver}&trusted_connection=yes"
     engine = create_engine(connection_string, fast_executemany=True)
     return engine.connect()
 
 
 def get_bi_connection(
-    server="powerbi://api.powerbi.com/v1.0/myorg/BI-WIP Data Quality & Support",
-    database="Data Flow Metrics",
-):
-    """Returns a connection to the CarharttBI database."""
-    # POWER BI CONNECTION ####################
+    server: str = "powerbi://api.powerbi.com/v1.0/myorg/BI-WIP Data Quality & Support",
+    database: str = "Data Flow Metrics",
+) -> adodbapi.Connection:
+    """Returns a connection to the Carhartt Power BI database.
+    Args:
+        server (str): The server name.
+        database (str): The database name.
+    Returns:
+        adodbapi.Connection: The connection to the Carhartt Power BI database."""
     connection_string = (
         f"Provider=MSOLAP.8; Data Source={server}; Initial Catalog={database};"
     )
