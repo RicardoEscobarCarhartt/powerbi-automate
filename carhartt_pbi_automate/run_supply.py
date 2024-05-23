2d21d83a025049230121242e8dc45adfa5e74d84
Jorge Ricardo Escobar Carrasco <rescobar@carhartt.com>
2024-05-23 19:55:52 -0400

    refactor: Rename app.py to run_supply.py and add argument parsing

--- carhartt_pbi_automate/run_supply.py
+++ carhartt_pbi_automate/run_supply.py
@@ -8,6 +8,7 @@ import threading
 from typing import List
 from pathlib import Path
 import traceback
+import argparse
 
 import pandas as pd
 import datacompy
@@ -19,7 +20,9 @@ from connector import get_edw_connection, get_bi_connection
 from popup import detect_popup_window
 from supply import get_edw_start_end_dates, get_dax_query, trim_dax_columns
 from get_logger import get_logger
-
+from dax import pass_args_to_dax_query
+from parse_arguments import parse_arguments
+from get_formated_duration import get_formated_duration
 
 # Create a logger object
 log = get_logger("carhartt_pbi_automate", "logs/carhartt_pbi_automate.log")
@@ -30,6 +33,19 @@ load_dotenv()
 # Define a global variable to store the connection to Power BI
 # To be accessed from a thread.
 conn_BI_result: List[adodbapi.Connection] = [None]
+
+# Parse script arguments
+try:
+    script_args = parse_arguments()
+    log.debug(f"Script arguments: {script_args}")
+    log.debug(f"DAX file path: {script_args.daxfile}")
+    log.debug(f"SQL file path: {script_args.sqlfile}")
+except argparse.ArgumentError as error:
+    stack_trace = traceback.format_exc()
+    log.error(f"Error: {error}")
+    log.error(f"Stack trace: {stack_trace}")
+    log.critical("Failed to parse script arguments. Exiting the program.")
+    exit(1)
 
 
 # Define a function that wraps get_bi_connection.
@@ -116,7 +132,7 @@ while True:
                 "Failed to connect. Exiting the program. Please check the logs for more information."
             )
             log.critical(f"Stack trace: {stack_trace}")
-            exit()
+            exit(1)
 
 log.info("Connection to Power BI has been established!")
 
@@ -128,7 +144,7 @@ log.info("Connection to Teams has been established!")
 # Load supply.sql file and read the query
 time_start = datetime.now()
 log.info("Extracting data from EDW...")
-supply_sql_filepath = Path("sql/supply.sql")
+supply_sql_filepath = Path(script_args.sqlfile)
 with open(supply_sql_filepath, "r", encoding="utf-8") as file:
     query_edw = file.read()
     df_edw = pd.read_sql(query_edw, conn_EDW)
@@ -136,13 +152,11 @@ time_end = datetime.now()
 time_diff = time_end - time_start
 
 # Calculate the time taken to extract data from EDW
-hours = time_diff.seconds // 3600
-minutes = (time_diff.seconds // 60) % 60
-seconds = time_diff.seconds % 60
+formated_duration = get_formated_duration(time_diff)
 
 # Print the time taken to extract data from EDW
 log.info("Data from EDW has been extracted.")
-log.debug(f"It took: {hours:02d}:{minutes:02d}:{seconds:02d}")
+log.debug(f"It took: {formated_duration} to extract data from EDW.")
 
 # Extract data from Power BI
 cursor_data_BI = conn_BI.cursor()
@@ -151,6 +165,9 @@ try:
     inventory_plans_start_month, inventory_plans_end_month = (
         get_edw_start_end_dates(df_edw)
     )
+    # Get the current date in the format "NIGHTLY-MM/DD/YYYY"
+    log.debug(f"Start month: {inventory_plans_start_month}")
+    log.debug(f"End month: {inventory_plans_end_month}")
 except ValueError as error:
     stack_trace = traceback.format_exc()
     # Send a message to Teams
@@ -166,25 +183,49 @@ except ValueError as error:
 
     log.critical(f"Data comparison failed: {error}")
     log.critical(f"Stack trace: {stack_trace}")
-    exit()
+    exit(1)
 
-# Get the current date in the format "NIGHTLY-MM/DD/YYYY"
-log.debug(f"Start month: {inventory_plans_start_month}")
-log.debug(f"End month: {inventory_plans_end_month}")
+
 now = datetime.now()
-nightly_current_date = f'"NIGHTLY-{now.month}/{now.day}/{now.year}"'
-dax_query = get_dax_query(
-    inventory_plans_start_month,
-    inventory_plans_end_month,
-    nightly_current_date,
-)
+plan_versions = f'"NIGHTLY-{now.month}/{now.day}/{now.year}"'
+log.debug(f"Plan_versions: {plan_versions}")
+
+# Load the DAX query from file
+dax_query = Path(script_args.daxfile).read_text(encoding="utf-8")
+
+# Pass the arguments to the DAX query
+args = {
+    "inventory_plans_start_month": inventory_plans_start_month,
+    "inventory_plans_end_month": inventory_plans_end_month,
+    "plan_versions": plan_versions,
+}
+
+dax_query = pass_args_to_dax_query(dax_query, args)
 
 log.info("Extracting data from Power BI...")
 time_start = datetime.now()
 
 # Execute the DAX query
-cursor_data_BI.execute(dax_query)
-results_table = cursor_data_BI.fetchall()
+try:
+    cursor_data_BI.execute(dax_query)
+    results_table = cursor_data_BI.fetchall()
+except Exception as error:
+    stack_trace = traceback.format_exc()
+
+    # Send a message to Teams
+    myTeamsMessage.summary("Data comparison failed")
+    myTeamsMessage.text(f"Error: {error}")
+    myTeamsMessage.text(
+        f"""<font color='red'>Error: {error}</font><br>
+        There could be an outage in the Power BI database.<br>
+        Please check the logs for more information.<br>
+        """
+    )
+    myTeamsMessage.send()
+
+    log.critical(f"Data comparison failed: {error}")
+    log.critical(f"Stack trace: {stack_trace}")
+    exit(1)
 
 # Get the column names from the cursor, remove the brackets and create a list
 column_names = [
@@ -198,11 +239,11 @@ df_pbi = pd.DataFrame(data_table, columns=column_names)
 cursor_data_BI.close()
 time_end = datetime.now()
 time_diff = time_end - time_start
-hours = time_diff.seconds // 3600
-minutes = (time_diff.seconds // 60) % 60
-seconds = time_diff.seconds % 60
+
+# Calculate the time taken to extract data from Power BI
+formated_duration = get_formated_duration(time_diff)
 log.info("Data from Power BI has been extracted!")
-log.debug(f"Time to extract data from Power BI: {hours}:{minutes}:{seconds}")
+log.debug(f"Time to extract data from Power BI: {formated_duration}")
 
 # Remove uneccesary the columns in the power bi dataframe,
 # rename the column Year/Period/Month to YearPeriodMonth
@@ -258,7 +299,9 @@ if compare.matches():
     myTeamsMessage.text(full_message)
 
     # Log the message
-    log.info("Data comparison completed successfully! Message sent to Microsoft Teams.")
+    log.info(
+        "Data comparison completed successfully! Message sent to Microsoft Teams."
+    )
     log.debug(f"Microsoft Teams summary: {repr(SUMMARY)}")
     log.debug(f"Microsoft Teams message: {repr(MESSAGE)}")
 else:
@@ -277,7 +320,9 @@ else:
     myTeamsMessage.text(full_message)
 
     # Log the message
-    log.warning("Data comparison completed with differences! Message sent to Microsoft Teams.")
+    log.warning(
+        "Data comparison completed with differences! Message sent to Microsoft Teams."
+    )
     log.debug(f"Microsoft Teams summary: {repr(SUMMARY)}")
     log.debug(f"Microsoft Teams message: {repr(MESSAGE)}")
 
