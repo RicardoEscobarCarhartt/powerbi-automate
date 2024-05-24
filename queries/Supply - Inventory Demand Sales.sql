c855a660c20595bde93dea6e5a147928c0524258
Jorge Ricardo Escobar Carrasco <rescobar@carhartt.com>
2024-05-24 15:05:34 -0400

    Refactor DAX and SQL queries for supply and inventory demand sales
    This commit adds new DAX and SQL query files for the supply and inventory demand sales functionality. The DAX query file, `Supply - Inventory Demand Sales.msdax`, defines variables and filters for the query. The SQL query file, `Supply - Inventory Demand Sales.sql`, retrieves data related to sales demand units and other inventory metrics. These changes enhance the application's ability to analyze and manage supply and inventory demand sales data.

---
+++ queries/Supply - Inventory Demand Sales.sql
@@ -0,0 +1,31 @@
+DECLARE @VersionDateToValidate INT;
+SET @VersionDateToValidate =
+(
+    SELECT [DateKey]
+    FROM [CarharttDw].[Dimensions].[Days]
+    WHERE [CurrentDayOffset] = 0
+);
+
+----Sales Demand Units = SalesForecastUnits in EDW---
+SELECT TRIM([DT].[YearPeriodMonth]) AS "DatesYear/Period/Month",
+       SUM([SCP].[ConstrainedReceiptPlanUnits]) 'Constrained_Receipt_Plan_Units',
+       SUM([SCP].[CurrentTotalReceiptPlanUnits]) 'Total_Receipt_Plan_Units',
+       AVG([SCP].[TargetWeeksOfCoverage]) 'Forward_Weeks_Of_Coverage',
+       SUM([SCP].[PlannedProductionUnits]) 'Planned_Production_Units',
+       SUM([SCP].[WorkInProgressUnits]) 'Work_In_Progress_Units',
+       SUM([SCP].[InTransitUnits]) 'In_Transit_Units',
+       SUM([SCP].[SalesForecastUnits]) 'Sales_Demand_Units'
+FROM [CarharttDw].[planning].[SizedWeeklyCombinedPlans] SCP
+    INNER JOIN [CarharttDw].[Dimensions].[Days] DT
+        ON [DT].[DateKey] = [SCP].[FiscalWeekDateKey]
+    INNER JOIN [CarharttDw].[Dimensions].[Products] P
+        ON [P].[ProductKey] = [SCP].[ProductKey]
+WHERE [SCP].[PlanType] = 'NIGHTLY'
+      AND [SCP].[VersionDateKey] = @VersionDateToValidate --Is Key for SavedPlanName
+      AND [DT].[CurrentMonthOffset] BETWEEN -1 AND 6
+      AND [SCP].[InventorySegment] <> 'ALL' --Visible
+      AND [P].[Licensed] <> 'Y' --Visible 
+      AND [P].[Licensed] IS NOT NULL --Hidden
+GROUP BY [DT].[YearPeriodMonth]
+ORDER BY [DT].[YearPeriodMonth];
+
