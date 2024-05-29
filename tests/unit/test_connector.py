dc5794d85e23cffb8ffe68ae5173196794bb97a7
Jorge Ricardo Escobar Carrasco <rescobar@carhartt.com>
2024-05-29 12:11:56 -0400

    chore: Add unit test for get_bi_connection function

--- tests/unit/test_connector.py
+++ tests/unit/test_connector.py
@@ -1,13 +1,13 @@
 """This module contains unit tests for the connector module."""
 
-import pytest
 from unittest.mock import Mock, patch
+
+import pytest
 
 from carhartt_pbi_automate.connector import (
     get_bi_connection,
     get_edw_connection,
 )
-
 
 
 @patch("carhartt_pbi_automate.connector.create_engine")
@@ -17,7 +17,6 @@ def test_get_edw_connection(mock_create_engine):
     mock_engine = Mock()
     mock_engine.connect.return_value = Mock()
     mock_create_engine.return_value = mock_engine
-
 
     # Arguments and expected result
     args = {
@@ -30,14 +29,40 @@ def test_get_edw_connection(mock_create_engine):
     # Act
     result = get_edw_connection(args)
 
-    # Assert
-    # engine = create_engine(connection_string, fast_executemany=True)
-    mock_create_engine.assert_called_with(expected_connection_string, fast_executemany=True)
+    # Asserts
+    # Assert create_engine was called with the expected connection string
+    mock_create_engine.assert_called_with(
+        expected_connection_string, fast_executemany=True
+    )
 
-    # Assert
-    # return engine.connect()
+    # Assert the result is the connection returned by the engine
     assert result == mock_engine.connect.return_value
-    
+
+
+@patch("carhartt_pbi_automate.connector.adodbapi.connect")
+def test_get_bi_connection(mock_connect):
+    """Tests the get_bi_connection function."""
+    # Mock create_engine return value
+    mock_connection = Mock()
+    mock_connect.return_value = mock_connection
+
+    # Arguments and expected result
+    server = "server"
+    database = "database"
+    expected_connection_string = (
+        f"Provider=MSOLAP.8; Data Source={server}; Initial Catalog={database};"
+    )
+
+    # Act
+    result = get_bi_connection(server, database)
+
+    # Asserts
+    # Assert the result is a connection
+    assert result == mock_connection
+
+    # Assert connect was called with the expected connection string
+    mock_connect.assert_called_with(expected_connection_string)
+
 
 if __name__ == "__main__":
     pytest.main()
