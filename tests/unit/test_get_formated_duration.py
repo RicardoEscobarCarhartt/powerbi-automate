66df88b1af203ef2a91a62f6b35b1485c6ee05ba
Jorge Ricardo Escobar Carrasco <rescobar@carhartt.com>
2024-05-31 14:01:11 -0400

    test: Add unit test for get_formated_duration_int function

--- tests/unit/test_get_formated_duration.py
+++ tests/unit/test_get_formated_duration.py
@@ -3,3 +3,11 @@
 import pytest
 
 from carhartt_pbi_automate.get_formated_duration import get_formated_duration
+
+
+@pytest.mark.unit
+def test_get_formated_duration_int():
+    """Test the get_formated_duration function."""
+    # test with a timedelta object
+    duration = get_formated_duration(3661)
+    assert duration == "01:01:01"
