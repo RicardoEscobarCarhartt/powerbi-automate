a9ab867ba08e689cd52957ce24b615f89bb3ef0c
Jorge Ricardo Escobar Carrasco <rescobar@carhartt.com>
2024-06-10 13:21:17 -0400

    refactor: Improve test_get_edw_connection function readability and maintainability

--- tests/unit/test_connector.py
+++ tests/unit/test_connector.py
@@ -10,35 +10,14 @@ from carhartt_pbi_automate.connector import (
 )
 
 
-@pytest.fixture(scope="function")
-def mock_create_engine():
-    """Fixture for mocking the create_engine function."""
-    with patch(
-        "carhartt_pbi_automate.connector.create_engine"
-    ) as _mock_create_engine:
-        yield _mock_create_engine
-
-
-@pytest.fixture(scope="function")
-def mock_engine_mock_create_engine(
-    mock_create_engine,
-):  # pylint: disable=W0621
-    """Fixture for mocking the create_engine function."""
-    # Mock create_engine return value
-    mock_engine = Mock()
-    mock_engine.connect.return_value = Mock()
-    mock_create_engine.return_value = mock_engine
-    yield mock_engine, mock_create_engine
-
-
 @pytest.mark.unit
 def test_get_edw_connection(
     mock_engine_mock_create_engine,
-):  # pylint: disable=W0621
+):
     """Tests the get_edw_connection function."""
     # Unpack fixtures
     mock_engine, mock_create_engine = (
-        mock_engine_mock_create_engine  # pylint: disable=W0621
+        mock_engine_mock_create_engine
     )
 
     # Arguments and expected result
