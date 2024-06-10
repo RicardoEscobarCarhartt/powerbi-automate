f0486ce4d6e83d4f3886f64565cc4526e68bf6e3
Jorge Ricardo Escobar Carrasco <rescobar@carhartt.com>
2024-06-10 13:40:13 -0400

    Refactor: add `mock_connect` and `mock_connection_mock_connect` fixtures to: tests\fixtures\connector.py

--- tests/fixtures/connector.py
+++ tests/fixtures/connector.py
@@ -4,6 +4,7 @@ from unittest.mock import Mock, patch
 
 import pytest
 
+
 @pytest.fixture(scope="function")
 def mock_create_engine():
     """Fixture for mocking the create_engine function."""
@@ -23,3 +24,23 @@ def mock_engine_mock_create_engine(
     mock_engine.connect.return_value = Mock()
     mock_create_engine.return_value = mock_engine
     yield mock_engine, mock_create_engine
+
+
+@pytest.fixture(scope="function")
+def mock_connect():
+    """Mock the adodbapi.connect function."""
+    with patch(
+        "carhartt_pbi_automate.connector.adodbapi.connect"
+    ) as mock_connect:  # pylint: disable=W0621
+        yield mock_connect
+
+
+@pytest.fixture(scope="function")
+def mock_connection_mock_connect(
+    mock_connect,
+):  # pylint: disable=W0621
+    """Fixture for mocking the adodbapi.connect function."""
+    # Mock connect return value
+    mock_connection = Mock()
+    mock_connect.return_value = mock_connection
+    yield mock_connection, mock_connect
